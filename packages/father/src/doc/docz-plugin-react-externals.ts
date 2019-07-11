07c7ba4f96b248eb64a4b91a10ff4f34dc694849
xchunzhao <xchunzhao@gmail.com>
2019-07-11 10:17:08 +0800

    feat: Add reactConfig for udf react&reactDom url

--- packages/father/src/doc/docz-plugin-react-externals.ts
+++ packages/father/src/doc/docz-plugin-react-externals.ts
@@ -13,11 +13,12 @@ export default function() {
       const flag = process.env.NODE_ENV === 'development' ? 'development' : 'production.min';
       config.htmlContext.head = config.htmlContext.head || ({} as any);
       config.htmlContext.head.scripts = config.htmlContext.head.scripts || [];
+      const reactConfig = config.reactConfig || ({} as any);
       config.htmlContext.head.scripts.push({
-        src: `https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.${flag}.js`,
+        src: reactConfig.react || `https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.${flag}.js`,
       });
       config.htmlContext.head.scripts.push({
-        src: `https://gw.alipayobjects.com/os/lib/react-dom/16.8.6/umd/react-dom.${flag}.js`,
+        src: reactConfig.reactDom || `https://gw.alipayobjects.com/os/lib/react-dom/16.8.6/umd/react-dom.${flag}.js`,
       });
       return config;
     },
