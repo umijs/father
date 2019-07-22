88389e03e09fa30d63c512bd92abba29275eb487
xchunzhao <xchunzhao@gmail.com>
2019-07-22 11:03:47 +0800

    fix(father-test): fix collectCoverageFrom not works

--- packages/father/src/test.ts
+++ packages/father/src/test.ts
@@ -1,9 +1,19 @@
 import umiTest from 'umi-test';
 import signale from 'signale';
+import { join } from 'path';
+import { existsSync } from 'fs';
 
 module.exports = async function test(args) {
-  const passArgs = {
+  const jestConfigFile = join(process.cwd(), 'jest.config.js');
+  let userJestConfig: any = {};
+  if (existsSync(jestConfigFile)) {
+    userJestConfig = require(jestConfigFile); // eslint-disable-line
+  }
+  const passArgs = userJestConfig.collectCoverageFrom ? {
     ...args,
+  } : {
+    collectCoverageFrom: 'src/**/*',
+    ...args
   };
 
   umiTest(passArgs).catch(e => {
