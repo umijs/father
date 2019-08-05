726fb75ff05c4cc7878436f28ebbd06f8af79f1c
zombiej <smith3816@gmail.com>
2019-08-05 11:49:41 +0800

    support config port

--- packages/father/src/doc/storybook.ts
+++ packages/father/src/doc/storybook.ts
@@ -2,7 +2,7 @@ import { DocProps } from '.';
 import storybook from '@storybook/react/standalone';
 import generator from './storybook-generator';
 
-export function devOrBuild({ cwd, cmd, DOC_PATH }: Partial<DocProps>) {
+export function devOrBuild({ cwd, cmd, DOC_PATH, args = {}, }: Partial<DocProps>) {
   const { storybookPath } = generator(cwd);
 
   if (cmd === 'build') {
@@ -15,7 +15,7 @@ export function devOrBuild({ cwd, cmd, DOC_PATH }: Partial<DocProps>) {
     // Dev mode
     return storybook({
       mode: 'dev',
-      port: '9001',
+      port: args.port || '9001',
       configDir: storybookPath,
     });
   }
