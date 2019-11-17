03ab24a8df0601bed3c77bee54ebb94cf2dad7a9
chenshuai2144 <qixian.cs@outlook.com>
2019-11-18 00:26:47 +0800

    bugfix: storybook port use number

--- packages/father/src/doc/storybook.ts
+++ packages/father/src/doc/storybook.ts
@@ -2,7 +2,7 @@ import { DocProps } from '.';
 import storybook from '@storybook/react/standalone';
 import generator from './storybook-generator';
 
-export function devOrBuild({ cwd, cmd, DOC_PATH, args = {}, }: Partial<DocProps>) {
+export function devOrBuild({ cwd, cmd, DOC_PATH, args = {} }: Partial<DocProps>) {
   const { storybookPath } = generator(cwd);
 
   if (cmd === 'build') {
@@ -17,7 +17,7 @@ export function devOrBuild({ cwd, cmd, DOC_PATH, args = {}, }: Partial<DocProps>
 
     return storybook({
       mode: 'dev',
-      port: args.port || '9001',
+      port: args.port || 9001,
       configDir: storybookPath,
     });
   }
