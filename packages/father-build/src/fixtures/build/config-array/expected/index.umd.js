5419853848eb35d268de7a083de21754624c920f
chencheng <sorrycc@gmail.com>
2019-12-05 22:52:12 +0800

    fix ci

--- packages/father-build/src/fixtures/build/config-array/expected/index.umd.js
+++ packages/father-build/src/fixtures/build/config-array/expected/index.umd.js
@@ -2,7 +2,7 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global = global || self, global.foo = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
 	var index = (function () {
 	  return 1;
@@ -10,4 +10,4 @@
 
 	return index;
 
-}));
+})));
--- packages/father-build/src/fixtures/build/rollup-commonjs-deps/expected/index.umd.js
+++ packages/father-build/src/fixtures/build/rollup-commonjs-deps/expected/index.umd.js
@@ -1,7 +1,7 @@
 (function (factory) {
   typeof define === 'function' && define.amd ? define(factory) :
   factory();
-}(function () { 'use strict';
+}((function () { 'use strict';
 
   var foo = {
     a: function () {
@@ -17,4 +17,4 @@
   console.log(foo_1());
   console.log(foo_2());
 
-}));
+})));
