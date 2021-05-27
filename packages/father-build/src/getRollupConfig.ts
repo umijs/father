930af789db2fdc9230ab43bf6753905daaec8cc3
zhanba <c5e1856@gmail.com>
2021-05-27 20:19:51 +0800

    feat: support dynamic import with multi-entry in esm mode  (#363)
    * fix: 支持配置outputdir
    
    * fix: change outputDir to dir
    
    * fix: add test
    
    * fix: update config

--- packages/father-build/src/getRollupConfig.ts
+++ packages/father-build/src/getRollupConfig.ts
@@ -215,12 +215,22 @@ export default function(opts: IGetRollupConfigOpts): RollupOptions[] {
 
   switch (type) {
     case 'esm':
+      let output: Record<string, any> = {
+        file: join(cwd, `dist/${(esm && (esm as any).file) || `${name}.esm`}.js`),
+      }
+      // https://rollupjs.org/guide/en/#code-splitting
+      if (entry.length > 1) {
+        output = {
+          dir: (esm && (esm as any).dir) || "dist",
+          entryFileNames: `${name}.esm.js`,
+        }
+      }
       return [
         {
           input,
           output: {
             format,
-            file: join(cwd, `dist/${(esm && (esm as any).file) || `${name}.esm`}.js`),
+            ...output,
           },
           plugins: [...getPlugins(), ...(esm && (esm as any).minify ? [terser(terserOpts)] : [])],
           external: testExternal.bind(null, external, externalsExclude),
--- packages/father-build/src/schema.ts
+++ packages/father-build/src/schema.ts
@@ -26,6 +26,7 @@ export default {
             importLibToEs: {
               type: 'boolean',
             },
+            dir: noEmptyStr
           },
         },
       ],
