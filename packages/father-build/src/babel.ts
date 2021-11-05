e079d747a5795962b4930cc4c236ffcfc7747457
wondaywang <wondaywang@tencent.com>
2021-11-06 04:01:34 +0800

    bebal 增加alias识别

--- packages/father-build/src/babel.ts
+++ packages/father-build/src/babel.ts
@@ -9,6 +9,7 @@ import slash from "slash2";
 import * as chokidar from "chokidar";
 import * as babel from "@babel/core";
 import gulpTs from "gulp-typescript";
+import alias from "@gulp-plugin/alias";
 import gulpLess from "gulp-less";
 import gulpPlumber from "gulp-plumber";
 import gulpIf from "gulp-if";
@@ -166,6 +167,12 @@ export default async function (opts: IBabelOpts) {
         base: srcPath,
       })
       .pipe(watch ? gulpPlumber() : through.obj())
+      .pipe(
+        gulpIf((f) =>
+          tsConfig && isTsFile(f.path),
+          tsConfig.paths ? alias({ config: tsConfig, cwd }) : through.obj()
+        )
+      )
       .pipe(
         gulpIf((f) => !disableTypeCheck && isTsFile(f.path), gulpTs(tsConfig))
       )
