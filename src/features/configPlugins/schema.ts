ce9d674e7abc15e9abc79a6d5ba752e5651485e2
PeachScript <scdzwyxst@gmail.com>
2022-03-29 15:42:55 +0800

    feat: complete schema

--- src/features/configPlugins/schema.ts
+++ src/features/configPlugins/schema.ts
@@ -1,13 +1,54 @@
-import type { Root } from '@umijs/core/compiled/@hapi/joi';
+import type { Root, SchemaLike } from '@umijs/core/compiled/@hapi/joi';
+import { IFatherPlatformTypes } from '../../types';
+
+function getCommonSchemas(): Record<string, (Joi: Root) => any> {
+  return {
+    platform: (Joi) =>
+      Joi.equal(
+        IFatherPlatformTypes.BROWSER,
+        IFatherPlatformTypes.NODE,
+      ).default(IFatherPlatformTypes.BROWSER),
+    define: (Joi) =>
+      Joi.object().pattern(Joi.string(), Joi.string()).optional(),
+    alias: (Joi) => Joi.object().pattern(Joi.string(), Joi.string()).optional(),
+    postcssOptions: (Joi) => Joi.object().optional(),
+    autoprefixer: (Joi) => Joi.object().optional(),
+    extraBabelPresets: (Joi) => Joi.array().optional(),
+    extraBabelPlugins: (Joi) => Joi.array().optional(),
+  };
+}
+
+function getCommonSchemasJoi(Joi: Root) {
+  const schemas = getCommonSchemas();
+
+  return Object.keys(schemas).reduce((jois, key) => {
+    jois[key] = schemas[key](Joi);
+
+    return jois;
+  }, {} as Record<string, SchemaLike | SchemaLike[]>);
+}
 
 export function getSchemas(): Record<string, (Joi: Root) => any> {
   return {
+    ...getCommonSchemas(),
     esm: (Joi) =>
       Joi.object({
+        ...getCommonSchemasJoi(Joi),
         input: Joi.string(),
         output: Joi.string(),
         transformer: Joi.string(),
         overrides: Joi.object(),
+        ignores: Joi.array().items(Joi.string()),
+      }),
+    umd: (Joi) =>
+      Joi.object({
+        ...getCommonSchemasJoi(Joi),
+        entry: Joi.alternatives()
+          .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string()))
+          .optional(),
+        output: Joi.string().optional(),
+        externals: Joi.object().pattern(Joi.string(), Joi.string()),
+        chainWebpack: Joi.function().optional(),
       }),
   };
 }
