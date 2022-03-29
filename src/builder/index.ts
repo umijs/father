f1af954f9f7ba68367f02ab335ccf9426c82b965
PeachScript <scdzwyxst@gmail.com>
2022-03-29 16:38:25 +0800

    refactor: improve clean logic

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,10 +1,34 @@
+import { rimraf } from '@umijs/utils';
 import { IFatherConfig } from '../types';
 import bundle from './bundle';
 import bundless from './bundless';
 import { createConfigProviders } from './config';
 
+function getProviderOutputs(
+  providers: ReturnType<typeof createConfigProviders>,
+) {
+  const set = new Set<string>();
+  const types: (keyof typeof providers)[] = ['bundle', 'bundless'];
+
+  types.forEach((type) => {
+    providers[type]?.configs.forEach((config) => {
+      set.add(
+        typeof config.output === 'string' ? config.output : config.output!.path,
+      );
+    });
+  });
+
+  return Array.from(set);
+}
+
 export default async (opts: { userConfig: IFatherConfig; cwd: string }) => {
   const configProviders = createConfigProviders(opts.userConfig);
+  const outputs = getProviderOutputs(configProviders);
+
+  // clean output directories
+  outputs.forEach((output) => {
+    rimraf.sync(output);
+  });
 
   // TODO: register config change handler
 
