3f6c5cfc070314db70fe72e5845e7064d5864fcb
PeachScript <scdzwyxst@gmail.com>
2022-06-07 16:15:53 +0800

    chore: exclude node 17

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 export const MIN_NODE_VERSION = 14;
-export const EXCLUDE_NODE_VERSION = 15;
 export const DEFAULT_CONFIG_FILES = ['.fatherrc.ts', '.fatherrc.js'];
 export const FRAMEWORK_NAME = 'father';
