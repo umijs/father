3a278f40a9572992f9b891306d745a23e9787c19
PeachScript <scdzwyxst@gmail.com>
2022-06-21 23:39:52 +0800

    feat: handle exceptions for compiling dts

--- src/builder/bundless/dts/index.ts
+++ src/builder/bundless/dts/index.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { chalk } from '@umijs/utils';
 // @ts-ignore
 import tsPathsTransformer from '../../../../compiled/@zerollup/ts-transform-paths';
 
@@ -7,7 +8,9 @@ export default async function getDeclarations(
   opts: { cwd: string },
 ) {
   const output: { file: string; content: string; sourceFile: string }[] = [];
-  const ts = await import('typescript');
+  // use require() rather than import(), to avoid jest runner to fail
+  // ref: https://github.com/nodejs/node/issues/35889
+  const ts: typeof import('typescript') = require('typescript');
   const tsconfigPath = ts.findConfigFile(opts.cwd, ts.sys.fileExists);
 
   if (tsconfigPath) {
@@ -17,6 +20,17 @@ export default async function getDeclarations(
       ts.sys,
       path.dirname(tsconfigPath),
     );
+
+    // check tsconfig error
+    // istanbul-ignore-if
+    if (tsconfig.errors.length) {
+      throw new Error(
+        `Error parsing tsconfig.json content: ${chalk.redBright(
+          ts.flattenDiagnosticMessageText(tsconfig.errors[0].messageText, '\n'),
+        )}`,
+      );
+    }
+
     const tsHost = ts.createCompilerHost(tsconfig.options);
 
     tsHost.writeFile = (fileName, declaration, _a, _b, sourceFiles) => {
@@ -36,9 +50,22 @@ export default async function getDeclarations(
     // using ts-paths-transformer to transform tsconfig paths to relative path
     // reason: https://github.com/microsoft/TypeScript/issues/30952
     // ref: https://www.npmjs.com/package/@zerollup/ts-transform-paths
-    program.emit(undefined, undefined, undefined, true, {
+    const result = program.emit(undefined, undefined, undefined, true, {
       afterDeclarations: [tsPathsTransformer(program).afterDeclarations],
     });
+
+    // check compile error
+    // istanbul-ignore-if
+    if (result.diagnostics.length) {
+      throw new Error(
+        `Error compiling declarations: ${chalk.redBright(
+          ts.flattenDiagnosticMessageText(
+            result.diagnostics[0].messageText,
+            '\n',
+          ),
+        )}`,
+      );
+    }
   }
 
   return output;
