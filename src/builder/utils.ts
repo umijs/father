84cf2540db8231223314a5090313f7d531b9e2a1
cl1107 <cl_1107@126.com>
2022-08-10 11:41:17 +0800

    fix: dist inconsistent between dev and build (umijs/father-next#59)
    * fix: 将NODE_ENV添加到cacheKey
    
    * fix: set development false
    
    * Update src/builder/utils.ts
    
    Co-authored-by: Peach <scdzwyxst@gmail.com>

--- src/builder/utils.ts
+++ src/builder/utils.ts
@@ -13,6 +13,9 @@ export function getBabelPresetReactOpts(pkg: IApi['pkg']) {
     const isLTRReact17 = semver.subset(reactVer, '>=17.0.0-0');
 
     opts = {
+      // force use production mode, to make sure dist of dev/build are consistent
+      // ref: https://github.com/umijs/umi/blob/6f63435d42f8ef7110f73dcf33809e6cda750332/packages/babel-preset-umi/src/index.ts#L45
+      development: false,
       // use legacy jsx runtime for react@<17
       runtime: isLTRReact17 ? 'automatic' : 'classic',
       ...(isLTRReact17 ? {} : { importSource: undefined }),
