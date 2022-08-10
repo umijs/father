fff35bd7bb16fab8bd9dc1468d7b711d1e9d7562
赤敛 <chilian@myweimai.com>
2022-08-10 16:11:23 +0800

    fix: importLibToEs add ExportAllDeclaration

--- packages/father-build/src/importLibToEs.js
+++ packages/father-build/src/importLibToEs.js
@@ -19,6 +19,7 @@ function replaceLib() {
     visitor: {
       ImportDeclaration: replacePath,
       ExportNamedDeclaration: replacePath,
+      ExportAllDeclaration: replacePath,
     },
   };
 }
