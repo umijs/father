b724a88230f4a6ac16076c06bbac5b242bb7581f
miracles1919 <lujie@yuantutech.com>
2022-09-12 23:00:02 +0800

    feat: support generate commitlint (#507)
    * feat: support generate commitlint
    
    * feat: 1
    
    * chore: add fast-glob
    
    * refactor: adjust
    
    * test: add test

---
+++ src/commands/generators/commitlint.ts
@@ -0,0 +1,67 @@
+import { GeneratorType } from '@umijs/core';
+import { getNpmClient, logger } from '@umijs/utils';
+import { execSync } from 'child_process';
+import fg from 'fast-glob';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { IApi } from '../../types';
+import { GeneratorHelper } from './utils';
+
+export default (api: IApi) => {
+  api.describe({
+    key: 'generator:commitlint',
+  });
+
+  api.registerGenerator({
+    key: 'commitlint',
+    name: 'Enable Commitlint',
+    description: 'Setup Commitlint Configuration',
+    type: GeneratorType.enable,
+    checkEnable: () => {
+      const pkg = JSON.parse(
+        readFileSync(join(api.paths.cwd, 'package.json'), 'utf-8'),
+      );
+      return (
+        ['.commitlintrc?(.*)', 'commitlint.config.*'].every(
+          (pattern) =>
+            fg.sync(pattern, {
+              cwd: api.paths.cwd,
+            }).length === 0,
+        ) && !pkg['commitlint']
+      );
+    },
+    disabledDescription:
+      'Commitlint has already enabled. You can remove commitlint config, then run this again to re-setup.',
+    fn: async () => {
+      const inGit = existsSync(join(api.paths.cwd, '.git'));
+      if (!inGit) {
+        logger.warn('仅 git 仓库下可用');
+        return;
+      }
+
+      const h = new GeneratorHelper(api);
+
+      const deps = {
+        '@commitlint/cli': '^17.1.2',
+        '@commitlint/config-conventional': '^17.1.0',
+        husky: '^8.0.1',
+      };
+
+      h.addDevDeps(deps);
+      h.addScript('prepare', 'husky install');
+
+      api.pkg['commitlint'] = {
+        extends: ['@commitlint/config-conventional'],
+      };
+      writeFileSync(api.pkgPath, JSON.stringify(api.pkg, null, 2));
+      logger.info('Update package.json for commitlint');
+
+      h.installDeps();
+      const npmClient = getNpmClient({ cwd: api.cwd });
+      execSync(
+        `${npmClient} husky add .husky/commit-msg '${npmClient} commitlint --edit $1'`,
+      );
+      logger.info('Create a hook for commitlint');
+    },
+  });
+};
---
+++ tests/g.commitlint.test.ts
@@ -0,0 +1,94 @@
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  rmdirSync,
+  unlinkSync,
+  writeFileSync,
+} from 'fs';
+import path from 'path';
+import * as cli from '../src/cli/cli';
+import { GeneratorHelper } from '../src/commands/generators/utils';
+
+const mockInstall = jest.fn();
+jest
+  .spyOn(GeneratorHelper.prototype, 'installDeps')
+  .mockImplementation(mockInstall);
+
+const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+const mockExecSync = jest.fn();
+jest.doMock('child_process', () => {
+  const originalModule = jest.requireActual('child_process');
+  return {
+    __esModule: true,
+    ...originalModule,
+    execSync: mockExecSync,
+  };
+});
+
+const CASES_DIR = path.join(__dirname, 'fixtures/generator');
+
+describe('lint generator', () => {
+  const commitlintConfPath = path.join(CASES_DIR, 'commitlint.config.js');
+  process.env.APP_ROOT = path.join(CASES_DIR);
+
+  beforeAll(() => {
+    const git = path.join(CASES_DIR, '.git');
+    if (!existsSync(git)) {
+      mkdirSync(path.join(CASES_DIR, '.git'));
+    }
+  });
+
+  afterEach(() => {
+    if (existsSync(commitlintConfPath)) {
+      unlinkSync(commitlintConfPath);
+    }
+    writeFileSync(path.join(CASES_DIR, 'package.json'), '{}');
+    warnSpy.mockReset();
+  });
+
+  test('g commitlint', async () => {
+    await cli.run({
+      args: { _: ['g', 'commitlint'], $0: 'node' },
+    });
+
+    const pkg = JSON.parse(
+      readFileSync(path.join(CASES_DIR, 'package.json'), 'utf-8'),
+    );
+
+    expect(pkg['commitlint']).toMatchObject({
+      extends: ['@commitlint/config-conventional'],
+    });
+    expect(pkg['scripts']).toMatchObject({
+      prepare: 'husky install',
+    });
+    expect(pkg['devDependencies']).toMatchObject({
+      '@commitlint/cli': '^17.1.2',
+      '@commitlint/config-conventional': '^17.1.0',
+      husky: '^8.0.1',
+    });
+    expect(mockInstall).toBeCalled();
+    expect(mockExecSync).toBeCalledWith(
+      `pnpm husky add .husky/commit-msg 'pnpm commitlint --edit $1'`,
+    );
+  });
+
+  test('warning when commitlint config exists', async () => {
+    writeFileSync(commitlintConfPath, '{}');
+    await cli.run({
+      args: { _: ['g', 'commitlint'], $0: 'node' },
+    });
+    expect(warnSpy.mock.calls[0][1]).toBe(
+      'Commitlint has already enabled. You can remove commitlint config, then run this again to re-setup.',
+    );
+    unlinkSync(commitlintConfPath);
+  });
+
+  test('warning when not used in git repo', async () => {
+    rmdirSync(path.join(CASES_DIR, '.git'));
+    await cli.run({
+      args: { _: ['g', 'commitlint'], $0: 'node' },
+    });
+    expect(warnSpy.mock.calls[0][1]).toBe('仅 git 仓库下可用');
+  });
+});
