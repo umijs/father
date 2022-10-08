9e1b9313f717dc6222c014bbe1cca7a204d826b0
PeachScript <scdzwyxst@gmail.com>
2022-10-08 10:49:01 +0800

    refactor: doctor rule allow import self pkg

--- src/doctor/rules/PHANTOM_DEPS.ts
+++ src/doctor/rules/PHANTOM_DEPS.ts
@@ -12,6 +12,7 @@ export default (api: IApi) => {
 
       if (
         pkgName &&
+        api.pkg.name !== pkgName &&
         !api.pkg.dependencies?.[pkgName] &&
         !api.pkg.peerDependencies?.[pkgName] &&
         aliasKeys.every((k) => k !== i.path && !i.path.startsWith(`${k}/`)) &&
