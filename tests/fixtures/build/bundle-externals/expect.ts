9329a027a76df1718a21653abde55d7264f99bf1
PeachScript <scdzwyxst@gmail.com>
2023-02-27 15:33:02 +0800

    refactor: allow advanced externals usage for umd

--- tests/fixtures/build/bundle-externals/expect.ts
+++ tests/fixtures/build/bundle-externals/expect.ts
@@ -1,3 +1,4 @@
 export default (files: Record<string, string>) => {
   expect(files['umd/index.min.js']).toContain('require("antd")');
+  expect(files['umd/index.min.js']).toContain('require("sub").module');
 };
