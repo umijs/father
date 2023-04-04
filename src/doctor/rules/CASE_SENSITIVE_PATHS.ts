f8b0fd074204adcbdaa68b0cd4b5334096e524e5
PeachScript <scdzwyxst@gmail.com>
2023-04-04 11:39:32 +0800

    refactor: also check dependency import

--- src/doctor/rules/CASE_SENSITIVE_PATHS.ts
+++ src/doctor/rules/CASE_SENSITIVE_PATHS.ts
@@ -10,48 +10,54 @@ export default (api: IApi) => {
   let resolver: ReturnType<typeof enhancedResolve['create']['sync']>;
   let aliasKeys: string[];
 
-  // init checker
+  // set fs for checker
   checker.fs = fs;
-  checker.context = api.cwd;
 
   api.addImportsCheckup(async ({ file, imports, mergedAlias }) => {
     const errors: IDoctorReport = [];
 
     resolver ??= enhancedResolve.create.sync({
-      extensions: ['.ts', '.js', '.tsx', '.jsx', '.json'],
+      // handle source extensions
+      extensions: ['.ts', '.js', '.tsx', '.jsx', '.json', '.node'],
+      mainFields: ['module', 'main', 'browser'],
+      // keep path clear in cnpm/pnpm project
+      symlinks: false,
       alias: mergedAlias,
     });
     aliasKeys ??= Object.keys(mergedAlias);
 
     for (const i of imports) {
-      if (
-        i.path.startsWith('.') ||
-        aliasKeys.some((k) => k === i.path || i.path.startsWith(`${k}/`))
-      ) {
-        let res: ReturnType<typeof resolver> = false;
+      let res: ReturnType<typeof resolver> = false;
 
-        // try to resolve import to absolute file path
+      // try to resolve import to absolute file path
+      try {
+        res = resolver(i.resolveDir, i.path);
+      } catch {
+        /* skip if module not found */
+      }
+
+      // check case sensitive
+      if (res) {
+        // why ignore next?
+        // because coverage will run on linux, and linux is case-sensitive
+        /* istanbul ignore next -- @preserve */
         try {
-          res = resolver(i.resolveDir, i.path);
-        } catch {
-          /* skip if module not found */
-        }
-
-        // check case sensitive
-        if (res && checker.isCheckable(res)) {
-          try {
-            await checker.checkFileExistsWithCase(res);
-          } catch (e: any) {
-            errors.push({
-              type: 'error',
-              problem: `${e.message
-                .replace(/\[.+?\] /, '')
-                .replace(/`.+?`/, `\`${i.path}\``)}
-            ${chalk.gray(`at ${file}`)}`,
-              solution:
-                'Make sure that import path and filesystem path are exactly the same',
-            });
-          }
+          checker.context =
+            // for npm package, use package root path as context, to avoid check node_modules/*
+            res.match(/^.+node_modules[\/](?:@[^\/]+[\/])?[^\/]+/)?.[0] ||
+            // for local file, use cwd as context
+            api.cwd;
+          await checker.checkFileExistsWithCase(res);
+        } catch (e: any) {
+          errors.push({
+            type: 'error',
+            problem: `${e.message
+              .replace(/\[.+?\] /, '')
+              .replace(/`.+?`/, `\`${i.path}\``)}
+          ${chalk.gray(`at ${file}`)}`,
+            solution:
+              'Make sure that import path and filesystem path are exactly the same',
+          });
         }
       }
     }
