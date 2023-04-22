30c82df8f501fb340ca3710f5be0749d04068c6c
PeachScript <scdzwyxst@gmail.com>
2023-04-22 18:39:39 +0800

    test: update case for swc helper update

--- tests/fixtures/build/bundless-swc-targets/expect.ts
+++ tests/fixtures/build/bundless-swc-targets/expect.ts
@@ -1,4 +1,4 @@
 export default (files: Record<string, string>) => {
   expect(files['cjs/index.js']).toContain('await');
-  expect(files['esm/index.js']).toContain('_asyncToGenerator');
+  expect(files['esm/index.js']).toContain('_async_to_generator');
 };
